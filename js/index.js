8c91ba263eb8cbb4dd4aae3a2e925d1d56fbe0f0
Alphonzo Escolar <phonz.dev@gmail.com>
2021-04-22 21:31:02 +0800

    Add animation and refactor

--- js/index.js
+++ js/index.js
@@ -1,26 +1,14 @@
 const buttons = document.querySelectorAll(".card__btn");
 const car = document.querySelectorAll(".card__image");
 
-buttons[0].addEventListener("mouseenter", () => {
-  car[0].classList.toggle("shake");
-});
-
-buttons[0].addEventListener("mouseleave", () => {
-  car[0].classList.toggle("shake");
-});
-
-buttons[1].addEventListener("mouseenter", () => {
-  car[1].classList.toggle("shake");
-});
-
-buttons[1].addEventListener("mouseleave", () => {
-  car[1].classList.toggle("shake");
-});
-
-buttons[2].addEventListener("mouseenter", () => {
-  car[2].classList.toggle("shake");
-});
-
-buttons[2].addEventListener("mouseleave", () => {
-  car[2].classList.toggle("shake");
-});
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("mouseenter", () => {
+    car[i].classList.toggle("shake");
+  });
+  buttons[i].addEventListener("mouseleave", () => {
+    car[i].classList.toggle("shake");
+  });
+  buttons[i].addEventListener("click", () => {
+    car[i].classList.toggle("move");
+  });
+}
